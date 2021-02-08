3019685b6d283a15271708198ca39c46f361e252
vladdominator <73780858+vladdominator@users.noreply.github.com>
2021-02-08 05:46:36 +0300

    feat: for all values

--- src/index.js
+++ src/index.js
@@ -5,5 +5,14 @@ module.exports = function towelSort (matrix) {
    if(arguments.length === 0){
       return [];
    }
-
+   let arrValue = [];
+   for(let i = 0; i < matrix.length; i++){
+      if(i % 2 != 0){
+         matrix[i].reverse();
+      }
+   }
+   for(let i = 0; i < matrix.length; i++){
+      arrValue.push(...matrix[i]);
+   }
+   return arrValue;
 }
